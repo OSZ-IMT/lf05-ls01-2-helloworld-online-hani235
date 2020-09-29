c57819bca077485ad8511b15530cec70794b9424
Hanh Nguyen <hnguyen@allinsight.net>
2020-09-29 14:26:24 +0200

    new

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,6 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-
-        System.out.println("Guten Tag Duc Hanh Nguyen\nWillkommen in der Veranschtaltung \"Strukturierte Programmierung\".");
+        System.out.println("Guten Tag Duc Hanh Nguyen\nWillkommen in der Veranstaltung \"Strukturierte Programmierung\".");
     }
 }
